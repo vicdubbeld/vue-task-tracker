dd9bc750fe2de4b62ba86ef181c514bb62a0b41b
vicdubbeld <vicdubbeld@gmail.com>
2021-10-24 14:28:30 -0700

    firebase setup in prog

--- vue-crash-2021/src/main.js
+++ vue-crash-2021/src/main.js
@@ -5,6 +5,7 @@ import router from "./router";
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getDatabase, ref, set } from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -14,6 +15,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAEfKrYJlXmCkbgyi2Z-nTBbNePkK45nwA",
   authDomain: "vue-task-tracker.firebaseapp.com",
   projectId: "vue-task-tracker",
+  databseURL: "https://vue-task-tracker-default-rtdb.firebaseio.com/",
   storageBucket: "vue-task-tracker.appspot.com",
   messagingSenderId: "947499355173",
   appId: "1:947499355173:web:0033d998debb08c5e503cc",
@@ -23,6 +25,17 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const database = getDatabase(app);
+
+function writeUserData(id, text, day, reminder) {
+  const db = getDatabase();
+  set(ref(db, "tasks/" + id), {
+    id: id,
+    text: text,
+    day: day,
+    reminder: reminder,
+  });
+}
 
 createApp(App)
   .use(router)
