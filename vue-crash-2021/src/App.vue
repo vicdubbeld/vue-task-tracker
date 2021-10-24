19207fa7f09c066b63d01ba88f937a5905c36405
vicdubbeld <vicdubbeld@gmail.com>
2021-10-23 23:14:56 -0700

    GET, POST, and DELETE methods added. db.json is storing data and ui is updating accordingly

--- vue-crash-2021/src/App.vue
+++ vue-crash-2021/src/App.vue
@@ -31,13 +31,27 @@ export default {
       //toggles this form so that if it is true, it will change to false when clicked
       this.showAddTask = !this.showAddTask
     },
-    addTask(task){
-      this.tasks = [...this.tasks, task]
+    async addTask(task){
+      const res = await fetch('api/tasks', {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify(task),
+      })
+      const data = await res.json()
+      this.tasks = [...this.tasks, data]
     },
-    deleteTask(id) {
+    async deleteTask(id) {
       // for each task that does not equal the task id above ^ keep it all except for the task with that exact id.
       if(confirm('Are you sure?')){
-      this.tasks = this.tasks.filter((task) => task.id !== id)
+        const res = await fetch(`api/tasks/${id}`, {
+        method: "DELETE"
+        })
+        
+        res.status === 200 ? (this.tasks = this.tasks.filter((task) => task.id !== id)) : alert('Error deleting task')
+        
+      
       }
     },
     toggleReminder(id) {
@@ -49,12 +63,18 @@ export default {
       this.tasks = this.tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task)
     },
     async fetchTasks() {
-      const res = await fetch('http://localhost:5000/tasks')
+      const res = await fetch('api/tasks')
 
       const data = await res.json()
 
       return data
-    }
+    },
+    async fetchTask(id) {
+      const res = await fetch(`api/tasks/${id}`)
+      const data = await res.json()
+
+      return data
+    },    
   },
  async created() {
     this.tasks = await this.fetchTasks()
